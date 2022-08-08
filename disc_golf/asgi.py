3c17d68d690d08ba7fa91a2ca21137e9c127a801
FilipWirsen <Filip.wirsen99@gmail.com>
2022-08-08 19:36:03 +0000

    Initial commit

---
+++ disc_golf/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for disc_golf project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'disc_golf.settings')
+
+application = get_asgi_application()
