abd557b97cf15ac4c6c34cfb98e25b440d1ed9a6
FilipWirsen <Filip.wirsen99@gmail.com>
2022-08-11 14:17:29 +0000

    Added product_detail view and start on template

--- products/models.py
+++ products/models.py
@@ -15,9 +15,15 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    discount_percentage = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     raiting = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     
     def __str__(self):
         return f'{self.name} {self.category} {self.price}'
+
+    @property
+    def get_discounted_price(self):
+        self.price = self.price - self.price * self.discount_percentage
+        return self.price
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models.functions import Lower
 from django.db.models import Q
@@ -58,3 +58,11 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    context = {
+        'product': product,
+    }
+    return render(request, 'products/product_detail.html', context)
