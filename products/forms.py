f7a2c4475c2d48feeb77526dc5347c022ef40a18
FilipWirsen <Filip.wirsen99@gmail.com>
2022-08-12 18:14:00 +0000

    Added all toasts, created add_product view, template and url

--- products/forms.py
+++ products/forms.py
@@ -1,9 +1,9 @@
 from django import forms
-from .models import Product, Category
+from .models import Product
 
 
-class EditProductForm(forms.ModelForm):
-    """ Form to edit products """
+class ProductForm(forms.ModelForm):
+    """ Form to add and edit products """
 
     class Meta:
         model = Product
--- products/views.py
+++ products/views.py
@@ -5,7 +5,7 @@ from django.db.models.functions import Lower
 from django.db.models import Q
 
 from .models import Product, Category
-from .forms import EditProductForm
+from .forms import ProductForm
 
 # Create your views here.
 
@@ -70,14 +70,38 @@ def product_detail(request, product_id):
     return render(request, 'products/product_detail.html', context)
 
 
+@login_required()
+def add_product(request):
+    """ Add products to the store """
+    if not request.user.is_superuser:
+        messages.warning(request, 'Only admins are allowed there...')
+        return redirect(reverse('home'))
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        product = form.save()
+        if form.is_valid():
+            messages.success(request, f'Successfully created product, {product.name}')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'products/add_product.html', context)
+
+
+@login_required()
 def edit_product(request, product_id):
     """ Form to edit products """
     if not request.user.is_superuser:
-        messages.error(request, 'Only admins can do that...')
+        messages.warning(request, 'Only admins are allowed there...')
         return redirect(reverse('home'))
     product = get_object_or_404(Product, pk=product_id)
     if request.method == 'POST':
-        form = EditProductForm(request.POST, instance=product)
+        form = ProductForm(request.POST, request.FILES, instance=product)
         if form.is_valid():
             form.save()
             messages.success(request, f'Successfully updated {product.name}.')
@@ -85,8 +109,8 @@ def edit_product(request, product_id):
         else:
             messages.error(request, 'Failed to update product. Please ensure the form is valid.')
     else:
-        form = EditProductForm(instance=product)
-
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are currently editing product: {product.name}')
     context = {
         'form': form,
         'product': product
@@ -97,7 +121,7 @@ def edit_product(request, product_id):
 def delete_product(request, product_id):
     """ Deletes product from the store """
     if not request.user.is_superuser:
-        messages.error(request, 'Only admins can do that...')
+        messages.warning(request, 'Only admins can do that...')
         return redirect(reverse('home'))
     product = get_object_or_404(Product, pk=product_id)
     product.delete()
