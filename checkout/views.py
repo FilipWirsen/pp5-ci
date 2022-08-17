20d528716b51da3b2c276ea4d4a56c7e59e129f2
FilipWirsen <Filip.wirsen99@gmail.com>
2022-08-17 17:28:26 +0000

    Added basic checkout functionality

--- checkout/views.py
+++ checkout/views.py
@@ -1,19 +1,33 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from cart.context import cart_contents
+
+import stripe
 
 
 def checkout_view(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
     cart = request.session.get('cart', {})
     if not cart:
         messages.error(request, "There's nothing in your cart. Add products before trying to checkout.")
         return redirect(reverse('products'))
-
+    current_cart = cart_contents(request)
+    total = current_cart['grand_total']
+    # Stripe wont accept float so we round up total to nearest integer
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
     order_form = OrderForm()
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51LOdyoAgnFUG4JtXY9Yr5KBQ7aGQF96TAy9UzZkmgymNlwipTWjNoAsyliYrVlWZbT7ymYnQdufmbe4D0HLxMl2i00NZFQWrSf',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
     return render(request, 'checkout/checkout.html', context)
--- disc_golf/settings.py
+++ disc_golf/settings.py
@@ -171,10 +171,14 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-# Shipping
-
+# Stripe 
 FREE_SHIPPING_THRESHHOLD = 50
 STANDARD_SHIPPING_PRICE = 5
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
