e6ebe758f8c4ae0a16b972ab1f70cd49ac9c4ddf
FilipWirsen <Filip.wirsen99@gmail.com>
2022-08-17 13:27:43 +0000

    Added checkout view and start on template

--- checkout/forms.py
+++ checkout/forms.py
@@ -30,6 +30,6 @@ class OrderForm(forms.ModelForm):
                 placeholder = f'{placeholders[field]} *'
             else:
                 placeholder = placeholders[field]
-            self.fields.widget.attrs['placeholder'] = placeholder
-            self.fields.widget.attrs['class'] = 'stripe-style-input'
-            self.fields[field].labels = False
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import OrderForm
+
+
+def checkout_view(request):
+    cart = request.session.get('cart', {})
+    if not cart:
+        messages.error(request, "There's nothing in your cart. Add products before trying to checkout.")
+        return redirect(reverse('products'))
+
+    order_form = OrderForm()
+    context = {
+        'order_form': order_form,
+    }
+    return render(request, 'checkout/checkout.html', context)
