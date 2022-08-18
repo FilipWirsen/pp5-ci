0745b261255de2bc70af79b234629d66cd181687
FilipWirsen <Filip.wirsen99@gmail.com>
2022-08-18 13:27:09 +0000

    Finished webhook handler

--- checkout/admin.py
+++ checkout/admin.py
@@ -11,12 +11,14 @@ class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderLineItemAdminInline,)
     readonly_fields = ('order_number', 'date',
                        'shipping_cost', 'order_total',
-                       'grand_total')
+                       'grand_total', 'original_cart',
+                       'stripe_pid',)
     
     fields = ('order_number', 'date', 'full_name', 'email',
               'phone_number', 'country', 'postcode', 'town_or_city',
               'street_address1', 'street_address2', 'county',
-              'shipping_cost', 'order_total', 'grand_total',)
+              'shipping_cost', 'order_total', 'grand_total',
+              'original_cart', 'stripe_pid',)
 
     list_display = ('order_number', 'date', 'full_name', 'email',
                     'shipping_cost', 'order_total', 'grand_total',)
--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -109,7 +109,7 @@ form.addEventListener('submit', function(ev) {
             } else {
             // The payment has been processed!
                 if (result.paymentIntent.status === 'succeeded') {
-                form.submit();
+                    form.submit();
                 }
             }
         });
