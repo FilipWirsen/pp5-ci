f52862823644468ef550fe5628dd281606ba16e2
FilipWirsen <Filip.wirsen99@gmail.com>
2022-08-18 11:47:38 +0000

    Added loading overlay for checkouts

--- checkout/forms.py
+++ checkout/forms.py
@@ -20,7 +20,7 @@ class OrderForm(forms.ModelForm):
             'country': 'Country',
             'postcode': 'Postal Code',
             'town_or_city': 'Town or City',
-            'street_address1': 'Street Address',
+            'street_address1': 'Street Address 1',
             'street_address2': 'Street Address 2',
             'county': 'County',
         }
