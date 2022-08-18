82f891f21f961f41743b2cecb742cd876d569e6d
FilipWirsen <Filip.wirsen99@gmail.com>
2022-08-18 23:54:40 +0000

    Completed confirmation emails

--- disc_golf/settings.py
+++ disc_golf/settings.py
@@ -179,6 +179,7 @@ STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+DEFAULT_FROM_EMAIL = 'discgolf@example.com'
 
 
 # Default primary key field type
