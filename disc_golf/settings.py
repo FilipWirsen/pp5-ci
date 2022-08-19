f3b8c5cc635f08f6b91e87582c2cc9d44be1d81b
FilipWirsen <Filip.wirsen99@gmail.com>
2022-08-19 17:25:57 +0000

    Removed secret_key and set debug

--- disc_golf/settings.py
+++ disc_golf/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5t^ilotj%cp@o%4ykp%dxps*&i0y8o@do)+1qpefoq+_bg6dw6'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
