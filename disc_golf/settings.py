b0d3601a851b44aa9d5aad14212ec9cc48cd8567
FilipWirsen <Filip.wirsen99@gmail.com>
2022-08-19 18:08:24 +0000

    Fix bug to set debug correct

--- disc_golf/settings.py
+++ disc_golf/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = if 'DEVELOPMENT' in os.environ
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ci-pp5-e-commerce.herokuapp.com', 'localhost']
 
