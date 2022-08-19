12781dc7d918f2ed5d115210ed97195ee4a92aee
FilipWirsen <Filip.wirsen99@gmail.com>
2022-08-19 20:43:26 +0000

    Update typo

--- disc_golf/settings.py
+++ disc_golf/settings.py
@@ -215,7 +215,7 @@ if 'DEVELOPMENT' in os.environ:
     DEFAULT_FROM_EMAIL = 'discgolf@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_USE_TSL = True
+    EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
