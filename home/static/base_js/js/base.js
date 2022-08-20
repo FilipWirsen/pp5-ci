1d0fa7a4719f4257425cf6372ce1cdbe9ec5bbe1
FilipWirsen <Filip.wirsen99@gmail.com>
2022-08-20 03:01:11 +0000

    Move base template js to separate file

---
+++ home/static/base_js/js/base.js
@@ -0,0 +1,35 @@
+window.onscroll = function () { 
+    scrollFunction();
+    let scroll = this.scrollY;
+    hide_header(scroll);
+    document.getElementsByClassName('toast').toast('show');
+};
+
+    function scrollFunction() {
+        var BackToTopBtn = document.getElementById("btn-back-to-top");
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            BackToTopBtn.style.display = "block";
+        } else {
+            BackToTopBtn.style.display = "none";
+        }
+    }
+
+    function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
+
+    function hide_header(scrollposition) {
+        var MaxWidth = window.matchMedia("(max-width: 750px)")
+        if (MaxWidth.matches) {
+            var header = document.getElementById('header')
+            if (scrollposition < 500) {
+                header.classList.remove('d-none')
+                header.classList.add('d-block')
+            } else {
+                header.classList.remove('d-block')
+                header.classList.add('d-none')
+            }
+        }
+
+    }
